bbd5cc56ed581b1bd47f590cf4fd78148c0513d1
shishiro <dheervathshishiro@gmail.com>
2023-12-08 00:33:30 +0530

    added search functionality

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { createBook,getBook,showAllBooks, editBookDetails,deleteBook} = require('../controllers/books.js')
-const { auth,isAdmin } = require('../middleware/auth.js')
+const { createBook, getBook, showAllBooks, editBookDetails, deleteBook } = require('../controllers/books.js')
+const { auth, isAdmin } = require('../middleware/auth.js')
 
-router.get('/getBook/:id',auth,getBook)
-router.get('/getAllBooks',showAllBooks)
+router.get('/getBook/:id', getBook)
+router.get('/getAllBooks', showAllBooks)
 
 //Admin routes
-router.post('/createBook',createBook)
-router.put('/editBook/:id',auth,isAdmin,editBookDetails)
-router.delete('/deleteBook/:id',deleteBook)
+router.post('/createBook', createBook)
+router.put('/editBook/:id', auth, isAdmin, editBookDetails)
+router.delete('/deleteBook/:id', deleteBook)
 
 module.exports = router
--- frontend/src/components/BookPage.jsx
+++ frontend/src/components/BookPage.jsx
@@ -50,11 +50,21 @@ const BookPage = () => {
         }
     };
 
+    const fetchSimilarBooks = async () => {
+        try {
+            console.log("This is the Id", id)
+            const response = await axios.get(`http://localhost:5000/api/v1/category/getCategory/${id}/`)
+            console.log("similar Books", response.data)
+        } catch (error) {
+            console.log("Error while fetching the same books", error.message);
+        }
+    }
 
     React.useEffect(() => {
         if (!dataFetched) {
             fetchCardData();
         }
+        fetchSimilarBooks()
     }, [dataFetched, id]);
 
     // const book = {
@@ -139,7 +149,9 @@ const BookPage = () => {
                         {
                             book.availability === true && (
                                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Reserve Book
+                                    <NavLink to={`/book/${id}`}>
+                                        Reserve Book
+                                    </NavLink>
                                 </button>
                             )
                         }
